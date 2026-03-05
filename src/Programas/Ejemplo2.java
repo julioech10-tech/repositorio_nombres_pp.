acc5d47a6603a64e7b45f00dcfad47de6c7259ce
JULIO <JULIO@192.168.1.40>
2026-03-04 23:31:48 -0500

    Primer commit proyecto DataOps

---
+++ src/Programas/Ejemplo2.java
@@ -0,0 +1,11 @@
+package programas;
+
+public class Ejemplo2 {
+
+    public static void main(String[] args){
+
+        System.out.print("Hola todos...!");
+        System.out.println("Actualización enviada a GitHub");
+    }
+
+}
